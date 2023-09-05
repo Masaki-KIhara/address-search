de1dabf66a5ac873796c6d29b72f1eb3264c9c23
Masaki Kihara <masaki1224.1998@gmail.com>
2023-09-05 21:03:48 +0900

    rule追加

--- src/constants/list/addressValueList.ts
+++ src/constants/list/addressValueList.ts
@@ -1,7 +1,11 @@
 import { AddressListValueType } from "../../type/formValue";
 
 export const AddressValueList: AddressListValueType = [
-  { label: "郵便番号", name: "postCode", rules: { required: true } },
+  {
+    label: "郵便番号",
+    name: "postCode",
+    rules: { required: true, maxLength: 7, minLength: 7 },
+  },
   { label: "都道府県", name: "prefecture", rules: { required: true } },
   { label: "市区町村", name: "municipalities", rules: { required: true } },
 ];
